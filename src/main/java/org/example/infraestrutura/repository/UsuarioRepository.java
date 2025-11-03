5a18afde0ab5987819e7cda37a2bcb042239d66d
Kauã Felix <kfelixcosta11@gmail.com>
2025-11-03 20:40:56 -0300

    feat: adiciona método para listar usuários no repositório

--- src/main/java/org/example/infraestrutura/repository/UsuarioRepository.java
+++ src/main/java/org/example/infraestrutura/repository/UsuarioRepository.java
@@ -3,9 +3,13 @@ package org.example.infraestrutura.repository;
 import org.example.infraestrutura.Conexao;
 import org.example.model.Usuario;
 
+import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioRepository {
 
@@ -26,5 +30,32 @@ public class UsuarioRepository {
             ps.setString(2, usuario.getEmail());
             ps.executeUpdate();
         }
+    }
+
+    public List<Usuario> listarUsuarios() throws SQLException{
+
+        List<Usuario>usuarios = new ArrayList<>();
+
+        String sql = """
+                SELECT
+                id, 
+                nome,
+                email
+                """;
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement ps = conn.prepareStatement(sql)){
+
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                int idUsuario = rs.getInt("id");
+                String nome = rs.getString("nome");
+                String email = rs.getString("email");
+                Usuario usuario = new Usuario(idUsuario, nome, email);
+                usuarios.add(usuario);
+            }
+        }
+        return usuarios;
     }
 }
