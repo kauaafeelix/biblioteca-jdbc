9a01fbaab00361d118a3eca1b4b0030111200b0c
Kauã Felix <kfelixcosta11@gmail.com>
2025-11-03 20:06:29 -0300

    feat: adiciona menu para gerenciar livros na classe BibliotecaView

--- src/main/java/org/example/view/BibliotecaView.java
+++ src/main/java/org/example/view/BibliotecaView.java
@@ -19,5 +19,12 @@ public class BibliotecaView {
         return scNum.nextInt();
     }
 
+    public static void menuLivros(){
+        System.out.println("===== Gerenciar Livros =====");
+        System.out.println("1 - Cadastrar Livro");
+        System.out.println("2 - Listar Livros");
+        System.out.println("0 - Voltar ao Menu Principal");
+    }
+
 
 }
