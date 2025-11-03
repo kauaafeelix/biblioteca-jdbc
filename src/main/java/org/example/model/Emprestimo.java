4adc81a68c7de40eec11cd0df0b11eaa0aac0ada
Kauã Felix <kfelixcosta11@gmail.com>
2025-11-03 19:31:21 -0300

    feat: cria classe Emprestimo para criação da entidade com seus atributos

---
+++ src/main/java/org/example/model/Emprestimo.java
@@ -0,0 +1,64 @@
+package org.example.model;
+
+public class Emprestimo {
+    private int id;
+    private int usuarioId;
+    private int livroId;
+    private String dataEmprestimo;
+    private String dataDevolucao;
+
+    public Emprestimo(int id, int usuarioId, int livroId, String dataEmprestimo, String dataDevolucao) {
+        this.id = id;
+        this.usuarioId = usuarioId;
+        this.livroId = livroId;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public Emprestimo(int usuarioId, int livroId, String dataEmprestimo, String dataDevolucao) {
+        this.usuarioId = usuarioId;
+        this.livroId = livroId;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public int getLivroId() {
+        return livroId;
+    }
+
+    public void setLivroId(int livroId) {
+        this.livroId = livroId;
+    }
+
+    public String getDataEmprestimo() {
+        return dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(String dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public String getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(String dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+}
