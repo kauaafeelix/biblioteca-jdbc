2ca4ae2ecfcd158e9b9e2bf7dfe98f1adb1aabdb
Kauã Felix <kfelixcosta11@gmail.com>
2025-11-03 20:04:03 -0300

    feat: adiciona metodo para consultar os dados da tabela livros no banco de dados

--- src/main/java/org/example/infraestrutura/repository/LivroRepository.java
+++ src/main/java/org/example/infraestrutura/repository/LivroRepository.java
@@ -5,7 +5,10 @@ import org.example.model.Livro;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LivroRepository {
 
@@ -26,5 +29,36 @@ public class LivroRepository {
             ps.setInt(3, livro.getAnoPublicacao());
             ps.executeUpdate();
         }
+    }
+
+    public List<Livro> listarLivros() throws SQLException{
+
+        List <Livro> livros = new ArrayList<>();
+
+        String sql = """
+                SELECT 
+                id,
+                titulo,
+                autor,
+                ano_publicacao,
+                disponivel 
+                FROM livros
+                """;
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()){
+                int idLivro = rs.getInt("id");
+                String titulo = rs.getString("titulo");
+                String autor = rs.getString("autor");
+                int anoPublicacao = rs.getInt("ano_publicacao");
+                boolean disponivel = rs.getBoolean("disponivel");
+                Livro livro = new Livro(idLivro, titulo, autor, anoPublicacao, disponivel);
+                livros.add(livro);
+            }
+        }
+        return livros;
     }
 }
