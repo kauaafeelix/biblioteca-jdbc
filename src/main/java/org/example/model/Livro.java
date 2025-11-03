a2ffee948d3d4f14faca3137d54ae8066ff0300b
Kauã Felix <kfelixcosta11@gmail.com>
2025-11-03 19:31:27 -0300

    feat: cria classe Livro para criação da entidade com seus atributos

---
+++ src/main/java/org/example/model/Livro.java
@@ -0,0 +1,64 @@
+package org.example.model;
+
+public class Livro {
+    private int id;
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    private boolean disponivel;
+
+    public Livro(int id, String titulo, String autor, int anoPublicacao, boolean disponivel) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.disponivel = disponivel;
+    }
+
+    public Livro(String titulo, String autor, int anoPublicacao, boolean disponivel) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+        this.disponivel = disponivel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+}
