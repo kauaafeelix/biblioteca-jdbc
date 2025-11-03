c033ccf8d34650d8a45ff79bd440b6801fd45919
Kauã Felix <kfelixcosta11@gmail.com>
2025-11-03 19:31:39 -0300

    feat: adiciona classe TesteConexao para verificar a conexão com o banco de dados

---
+++ src/main/java/org/example/infraestrutura/TesteConexao.java
@@ -0,0 +1,14 @@
+package org.example.infraestrutura;
+
+import java.sql.SQLException;
+
+public class TesteConexao {
+    public static void main(String[] args) {
+        try {
+            Conexao.conectar();
+            System.out.println("Conexão bem-sucedida!");
+        } catch (SQLException e) {
+            System.out.println("Falha na conexão: " + e.getMessage());
+        }
+    }
+}
