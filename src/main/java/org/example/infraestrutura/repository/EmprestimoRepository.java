5fe38404f16ae54a71b8dbae40e67c6706345aac
Kauã Felix <kfelixcosta11@gmail.com>
2025-11-03 21:11:53 -0300

    feat: adiciona método para listar empréstimos no repositório

--- src/main/java/org/example/infraestrutura/repository/EmprestimoRepository.java
+++ src/main/java/org/example/infraestrutura/repository/EmprestimoRepository.java
@@ -6,7 +6,11 @@ import org.example.service.EmprestimoService;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmprestimoRepository {
 
@@ -27,5 +31,44 @@ public class EmprestimoRepository {
             ps.setInt(2, emprestimo.getUsuarioId());
             ps.executeUpdate();
         }
+    }
+
+    public List<Emprestimo>listarEmprestimos() throws SQLException{
+
+        String sql = """
+                SELECT
+                e.id,
+                l.nome AS nome_livro,
+                u.nome AS nome_usuario,
+                e.data_emprestimo,
+                e.data_devolucao
+                FROM emprestimos e
+                JOIN livros l ON e.id_livro = l.id
+                JOIN usuarios u ON e.id_usuario = u.id
+                """;
+
+        List<Emprestimo>emprestimos = new ArrayList<>();
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement ps = conn.prepareStatement(sql)){
+
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                int idEmprestimo = rs.getInt("id");
+                int nomeLivro = rs.getInt("nome_livro");
+                int nomeUsuario = rs.getInt("nome_usuario");
+                LocalDate dataEmprestimo = rs.getDate("data_emprestimo").toLocalDate();
+                LocalDate dataDevolucao = null;
+                if(rs.getDate("data_devolucao") != null){
+                    dataDevolucao = rs.getDate("data_devolucao").toLocalDate();
+                }
+
+                Emprestimo emprestimo = new Emprestimo(idEmprestimo, nomeLivro, nomeUsuario, dataEmprestimo, dataDevolucao);
+                emprestimos.add(emprestimo);
+            }
+        }
+
+        return emprestimos;
     }
 }
