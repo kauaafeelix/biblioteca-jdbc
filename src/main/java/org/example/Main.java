11b3f74f9e4c597763073ca0fdff1facdd534316
Kauã Felix <kfelixcosta11@gmail.com>
2025-11-06 21:20:35 -0300

    feat: implementa loop no menu principal para gerenciar usuários, livros e empréstimos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,44 @@
 package org.example;
 
+import org.example.model.Usuario;
+import org.example.service.EmprestimoService;
+import org.example.service.LivroService;
+import org.example.service.UsuarioService;
+import org.example.view.BibliotecaView;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        UsuarioService usuarioService = new UsuarioService();
+        LivroService livroService = new LivroService();
+        EmprestimoService emprestimoService = new EmprestimoService();
 
-        switch ()
+        int escolha = 0;
+
+        do {
+            var bibliotecaView = new BibliotecaView();
+            escolha = bibliotecaView.mostrarMenu();
+
+            switch (escolha) {
+
+                case 1 -> {
+                    usuarioService.gerenciadorUsuarios();
+                }
+                case 2 -> {
+                    livroService.gerenciadorLivros();
+                }
+                case 3 -> {
+                    emprestimoService.gerenciadorEmprestimos();
+                }
+                case 4 -> {
+                    System.out.println("Saindo do sistema. Até logo!");
+                }
+                default -> System.out.println("Opção inválida. Tente novamente.");
+
+            }
+        } while (escolha != 4);
+
     }
 }
