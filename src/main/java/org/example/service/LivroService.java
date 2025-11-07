f3fd5b0c05e8d33545d129ead3a4e8c7a7ec5f3d
Kauã Felix <kfelixcosta11@gmail.com>
2025-11-06 21:20:26 -0300

    feat: implementa loop no gerenciador de livros para melhorar a interação do usuário

--- src/main/java/org/example/service/LivroService.java
+++ src/main/java/org/example/service/LivroService.java
@@ -1,33 +1,66 @@
 package org.example.service;
 
+import org.example.infraestrutura.repository.LivroRepository;
+import org.example.model.Livro;
+import org.example.model.Usuario;
 import org.example.view.BibliotecaView;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LivroService {
 
     BibliotecaView bibliotecaView = new BibliotecaView();
-    int opcao = bibliotecaView.menuLivros();
+
+    int opcao = 0;
 
     public void gerenciadorLivros() {
-        switch (opcao) {
-            case 1 -> {
-                cadastrarLivro();
-            }
-            case 2 -> {
-                listarLivros();
-            }
-            case 3 -> {
-                bibliotecaView.mostrarMenu();
-            }
-            default -> System.out.println("Opção inválida. Tente novamente.");
 
+        do {
+            opcao = bibliotecaView.mostrarMenu();
+
+            switch (opcao) {
+                case 1 -> {
+                    cadastrarLivro();
+                }
+                case 2 -> {
+                    listarLivros();
+                }
+                case 3 -> {
+                    bibliotecaView.mostrarMenu();
+                }
+                default -> System.out.println("Opção inválida. Tente novamente.");
+
+            }
+        }while (opcao != 3);
+    }
+
+    public void cadastrarLivro() {
+        Livro livro = bibliotecaView.cadastrarLivro();
+
+        try{
+            var livroRepository = new LivroRepository();
+            livroRepository.inserirLivro(livro);
+            bibliotecaView.mensagemSucesso();
+        } catch (SQLException e) {
+            bibliotecaView.mensagemErroCadastro("livro");
+            e.printStackTrace();
         }
     }
 
-    public static void cadastrarLivro() {
-
-    }
-
-    public static void listarLivros() {
-
+    public void listarLivros() {
+        List<Usuario>usuarios = new ArrayList<>();
+        try {
+            var livroRepository = new LivroRepository();
+            var livros = livroRepository.listarLivros();
+            bibliotecaView.listarLivros(livros);
+        } catch (SQLException e) {
+            bibliotecaView.mensagemErro();
+            e.printStackTrace();
+        }
+        if (usuarios.isEmpty()) {
+            bibliotecaView.mensagemListaVazia("livro");
+        }
     }
 }
